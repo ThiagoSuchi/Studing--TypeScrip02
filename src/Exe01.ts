912603b6f8a8341bb0e7cc423488306f8c7df289
THIAGO <thiagohenssuchi@gmail.com>
2024-10-25 18:24:26 -0400

    Exercício 01 - Somo simples/basico

---
+++ src/Exe01.ts
@@ -0,0 +1,8 @@
+// Exercício 01: Crie uma função que receba dois números e retorne a soma deles.
+
+
+function somaNumeros(A: number,B: number):number {
+    return A + B
+}
+
+console.log(somaNumeros(10,2));
