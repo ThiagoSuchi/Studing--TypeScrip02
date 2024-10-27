1b5a36243b488788e864bf9b4a6f268f5d7a69af
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-10-27 19:14:22 -0400

    ...

---
+++ dist/Exe06.js
@@ -0,0 +1,21 @@
+"use strict";
+function divisivel(numA, numB) {
+    const resto = numA % numB;
+    if (resto == 0)
+        return true;
+    return false;
+}
+function ePrimo(num) {
+    for (let i = 2; i < num; i++) {
+        if (divisivel(num, i))
+            return false;
+    }
+    return true;
+}
+const numero = 881;
+if (ePrimo(numero)) {
+    console.log('é primo!');
+}
+else {
+    console.log('não é primo!');
+}
