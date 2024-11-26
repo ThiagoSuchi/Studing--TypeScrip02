44eea7443524d032f80a89a5f93392e7efed394b
THIAGO <thiagohenssuchi@gmail.com>
2024-11-25 22:33:25 -0400

    Task03 em andamento  - Gerenciamento de Eventos...

--- dist/Exe13.js
+++ dist/Exe13.js
@@ -1,19 +1,20 @@
-let letras = {
-    letraMaiuscula: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-    letraMinuscula: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-};
-function contagemDeLetras(string, letras) {
-    let str = string.split('');
-    let idx = 0;
-    for (let i = 0; i < letras.letraMinuscula.length; i++) {
-        if (str[idx] === letras.letraMinuscula[i]) {
-            console.log(str[idx]);
-            idx++;
-        }
-        else {
+function contagemDeLetras(string) {
+    let verificadorLetras = [];
+    const letra = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let l = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === 'o') {
+            verificadorLetras.push(string[i]);
         }
     }
+    let qtd = verificadorLetras.length;
+    for (let i = 0; i < letra.length; i++) {
+        if (letra[i] === verificadorLetras[l]) {
+            console.log(`${letra[i]} = ${qtd}`);
+        }
+    }
+    return verificadorLetras;
 }
-const doc = 'Ooo sol pediu a lua em casamento! E aaa luuuaa disse não sei não sei, me dá um tempo!';
-contagemDeLetras(doc, letras);
+const doc = 'ooo sol pediu a lua em casamento, e aaa luuuaa disse não sei não sei, me dá um tempo!';
+console.log(contagemDeLetras(doc));
 export {};
---
+++ src/tarefa03.ts
@@ -0,0 +1,8 @@
+/* 3) Gerenciamento de Eventos
+- Crie um sistema que exiba informações sobre eventos.
+- Defina uma interface Event com os campos: id, name, date (string).
+- Crie funções para gerenciar eventos (adicionar, listar, listar um específico e deletar).
+- Crie uma função que receba um array de eventos e exiba os eventos futuros (data maior que a atual).
+ */
+
+console.log("Hello World");
