2c099849516e87c365cb17ec49225fa44f64e336
THIAGO <thiagohenssuchi@gmail.com>
2024-11-27 18:39:02 -0400

    Tarefa Gerenciar Eventos, em andamento...

--- src/tarefa03.ts
+++ src/tarefa03.ts
@@ -4,38 +4,76 @@
 - Crie funções para gerenciar eventos (adicionar, listar, listar um específico e deletar).
 - Crie uma função que receba um array de eventos e exiba os eventos futuros (data maior que a atual).
 */
-import { v4 as uuidv4 } from "uuid"
 
-interface Event {
-    id: string,
+
+interface Evento {
+    id: number,
     name: string,
-    date: Date
+    date: Date | string
 }
 
-const eventos: Event[] = []
-
+const eventos: Evento[] = []
+let currentId = 0;
 
 // Adcionando eventos
-function adicionarEvent(event: Event[]) {
-    event.map((eve) => eventos.push(eve))
+function adicionarEvent(nome: string, data:string) {
+    // Gerador de id
+    currentId += 1
+
+    const arrayEventos: Evento[] = [
+        {
+         id: currentId,
+         name: nome,
+         date: data
+        }
+    ]
+    eventos.push(arrayEventos[0])
+}
+
+//Deletar o evento
+function deleteEvent(evento: Evento[], nome: Evento) {
+    let listaAtualizada = []
+    for(let i = 0, j = evento.length; i !== j; i++) {
+        if(evento[i] !== nome) listaAtualizada.push(evento[i])
+    }
+
+    return listaAtualizada;
+}
+
+// Filtrando eventos com datas futuras
+function eventFuturo() {
+    console.log(eventos[0].date);
+     
+}
+
+// Filtrando Eventos pelo id
+function filtrarEvento (idEvent: number, events: Evento[]) {
+    const eventoFiltrdo = events.filter((e) => {
+        return e.id === idEvent
+    })
+    return eventoFiltrdo
 }
 
 // Listando os eventos
-function listaEventos () {
-    const evento: Event[] = [
-        { id: uuidv4(), name: 'Show Morada/Banda Gospel', date: new Date('2024-11-23') },
-        { id: uuidv4(), name: 'Stand-up Murilo Couto', date: new Date('2024-10-24') },
-        { id: uuidv4(), name: 'Inauguração Shopping Vilhena-RO', date: new Date('2024-10-28') },
-        { id: uuidv4(), name: 'Stand-up Afonso Padilha', date: new Date('2024-02-10') },
-        { id: uuidv4(), name: 'Show Projeto Sola', date: new Date('2024-12-03') },
-        { id: uuidv4(), name: 'Inauguração Park Aquático', date: new Date('2024-11-27') },
-    ]
-    adicionarEvent(evento)
-    return evento
+function listaEventos (): Evento[] {
+    return eventos
 }
 
-console.log(`
-Evento: ${listaEventos()[0].name}
-Data: ${listaEventos()[0].date.getUTCDate()}/${listaEventos()[0].date.getUTCMonth() + 1}/${listaEventos()[0].date.getUTCFullYear()} 
-`);
+adicionarEvent('Venom3 Nos Cinemas', '12/09/2024')
+adicionarEvent('Show Projeto Sola', '01/02/2025')
+adicionarEvent('Stand-up Afonso Padilha', '24/09/2024')
+adicionarEvent('Inauguração Shopping', '11/11/2024')
+adicionarEvent('Casamento Amigos', '22/10/2024')
+
+console.log('--------------------------------------------------------------------------');
+console.log(listaEventos());
+console.log('-------------------- Lista Atualizada: --------------------------');
+console.log(deleteEvent(eventos, eventos[1]));
+console.log('------------------------- Evento Filtrado Pelo Id -----------------------');
+console.log(...filtrarEvento(3, eventos));
+console.log('--------------------------------------------------------------------------');
+
+
+
+ 
  
