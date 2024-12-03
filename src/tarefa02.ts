f45b463fb41aad04138252358e8f300907319766
THIAGO <thiagohenssuchi@gmail.com>
2024-12-03 17:38:29 -0400

    Gerenciamento de Produtos

--- src/tarefa02.ts
+++ src/tarefa02.ts
@@ -7,25 +7,7 @@
 Obs.: utilize o pacote npm chalk para formatar a saída no console.
 */
 
-import { v4 as uuidv4 } from 'uuid';
-
-interface Produto {
-    id: string,
-    nome: string,
-    preco: number
-}
-
-const produtos: Produto[] = [
-    {id: uuidv4(), nome: 'Parafusadeira/Furadeira de Impacto Brushless', preco: 649.00},
-    {id: uuidv4(), nome: 'Furadeira de Impacto', preco: 649.00},
-    {id: uuidv4(), nome: 'Esmirilhadeira Angular', preco: 620.90},
-    {id: uuidv4(), nome: 'Serra sabre Compacta 20V', preco: 560.90},
-    {id: uuidv4(), nome: 'Jogo de Chave Combinadada', preco: 69.00},
-    {id: uuidv4(), nome: 'Jogo de Chave Biela 8 a 19mm', preco: 253.90},
-    {id: uuidv4(), nome: 'Jogo de Alicate com 3 peças', preco: 87.90},
-    {id: uuidv4(), nome: 'Jogo de Chaves Estrela Catraca 6x21mm', preco: 99.90},
-    {id: uuidv4(), nome: 'Marreta Oitavada', preco: 274.90},
-];
+import { produtos } from "./data02/array";
 
 produtos.forEach((prod) => {
     console.log(`Produto: ${prod.nome} - (R$${prod.preco.toFixed(2)})
