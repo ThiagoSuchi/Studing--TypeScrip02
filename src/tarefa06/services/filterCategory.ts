36fe623077472080d52e74db3c0df409f9936304
THIAGO <thiagohenssuchi@gmail.com>
2024-12-05 17:30:47 -0400

    Manipulando dados com Map e Filter - Lista de produtos/aplicando disconto/e filtrando por categoria

--- src/tarefa06/services/filterCategory.ts
+++ src/tarefa06/services/filterCategory.ts
@@ -1,7 +1,7 @@
 import { Product } from "../models/interface";
 import { Category } from "../models/enum";
 
-export function filtrarPorCategoria(produtos: Product[], categoria: string) {
+export function filtrarPorCategoria(produtos: Product[], categoria: string): Product[] {
     // O Find - Retorna o primeiro elemento que atende à condição ou undefined.
     const proFiltrado = Object.values(Category).find((cat) => {
         return cat.toLocaleLowerCase() === categoria.toLocaleLowerCase()
